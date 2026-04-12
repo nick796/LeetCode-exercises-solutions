717c5e2bdf6394990a991f96560a9a61a19a2a97
nick796 <32782139+nick796@users.noreply.github.com>
2026-04-12 18:44:33 +0300

    Add files via upload

---
+++ 2620_Counter.js
@@ -0,0 +1,18 @@
+/**
+2620. Counter
+Given an integer n, return a counter function. 
+This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+ */
+var createCounter = function(n) {
+    let tinker = n
+    return function() {
+        return tinker++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
